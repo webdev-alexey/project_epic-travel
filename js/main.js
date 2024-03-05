b10d0d885a5213bf5788f0ee98244c52353d2e48
Alexey <alexey.anasov@yandex.by>
2024-03-05 23:09:29 +0300

    добавлена мобильная навигация

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Находим элементы
+const btn = document.querySelector("#btn-mobile-nav");
+const mobileBtnImg = document.querySelector("#mobile-nav-img");
+const mobileNav = document.querySelector("#mobile-nav");
+const header = document.querySelector("#header");
+
+// Добавляем прослушку по клику
+btn.addEventListener("click", function () {
+  console.log("Click");
+
+  btn.classList.toggle("open");
+  mobileNav.classList.toggle("open");
+  header.classList.toggle("header-white-bg");
+  document.body.classList.toggle("no-scroll");
+
+  if (btn.classList.contains("open")) {
+    mobileBtnImg.src = "./img/icons/close-mobile-nav.svg";
+  } else {
+    mobileBtnImg.src = "./img/icons/open-mobile-nav.svg";
+  }
+});
